7b7635741c446b28160b290c899c948c0fc783dd
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-01-07 23:27:32 +0900

    style: change eslinting rules to use arrow function

---
+++ src/component/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface HelloWorldProps {
+    name: string;
+}
+
+export const HelloWorld = ({ name } :HelloWorldProps) => (
+  <div>
+    {`Hello "${name}"`}
+  </div>
+);
+
+export default HelloWorld;
